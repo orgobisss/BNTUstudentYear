b4c05591036b04c1980753a18e0e6d7a92d47c69
egor <egortatur@yandex.by>
2024-11-18 14:28:25 +0300

    middle

--- app.py
+++ app.py
@@ -1,22 +1,64 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect, send_file
+from flask_sqlalchemy import SQLAlchemy
+from io import BytesIO
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///studentyear.db'
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    st_name = db.Column(db.Text, nullable=False)
+    num_group = db.Column(db.Integer, nullable=False)
+    faculty = db.Column(db.String(50), nullable=False)
+    main_text = db.Column(db.Text, nullable=False)
+    image = db.Column(db.LargeBinary)
+    like = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
 
 
 @app.route('/')
-@app.route('/home')
+@app.route('/index')
 def index():
-    return render_template("index.html")
+    articles = Article.query.all()
+    return render_template("index.html", articles=articles)
 
 
-@app.route('/about')
-def about():
-    return render_template("about.html")
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == "POST":
+        st_name = request.form['st_name']
+        num_group = request.form['num_group']
+        faculty = request.form['faculty']
+        main_text = request.form['main_text']
+        image = request.files['image']
+
+        # Если изображение было загружено, преобразуем его в бинарные данные
+        image_data = image.read() if image else None
+
+        new_article = Article(st_name=st_name, num_group=num_group, faculty=faculty, main_text=main_text, image=image_data)
+
+        try:
+            db.session.add(new_article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Error"
+    else:
+         return render_template("create-article.html")
 
 
-@app.route('/user/<string:name>/<int:id>')
-def about_user(name, id):
-    return "User page: " + name + " - " + str(id)
+@app.route('/image/<int:article_id>')
+def image(article_id):
+    # Извлекаем изображение для статьи
+    article = Article.query.get_or_404(article_id)
+    if article.image:
+        return send_file(BytesIO(article.image), mimetype='image/jpeg')
+    return "No image", 404
 
 
 if __name__ == "__main__":
